3469eb84dc590e2b5006541211534a3787285016
sirlao <sirlao.dev@gmail.com>
2024-11-12 17:46:08 -0300

    thirth commit for project despliegue

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { ghPages } from 'vite-plugin-gh-pages'
 
-const prueba=0
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), ghPages],
+  plugins: [vue(), ghPages()],
   base: "vue-despliegue"
 })

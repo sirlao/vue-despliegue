e7d94d6653f9610223c94ffea5ac483f5a301eac
sirlao <sirlao.dev@gmail.com>
2024-11-12 17:42:30 -0300

    second commit for project

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { ghPages } from 'vite-plugin-gh-pages'
 
+const prueba=0
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), ghPages],
